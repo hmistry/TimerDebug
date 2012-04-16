f8582951630069ed4b546adbc4f51c8d24b430f3
hiren mistry <hiren.mistry@chai-monsters.com>
2012-04-15 20:31:39 -0700

    Initial Commit

---
+++ TimerDebug/TimerDebugAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TimerDebugAppDelegate.h
+//  TimerDebug
+//
+//  Created by Hiren on 4/15/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimerDebugAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
