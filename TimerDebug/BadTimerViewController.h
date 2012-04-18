ee172f666f7343dbf5cd88dc89388327cf70590b
hiren mistry <hiren.mistry@chai-monsters.com>
2012-04-18 06:18:48 -0700

    Bad version working. Going to try different method of counting to see if effect is better.

--- TimerDebug/BadTimerViewController.h
+++ TimerDebug/BadTimerViewController.h
@@ -13,4 +13,20 @@
     
 }
 
+@property (nonatomic, retain) IBOutlet UILabel *timer1Label;
+@property (nonatomic, retain) IBOutlet UILabel *timer2Label;
+
+@property (nonatomic, retain) NSTimer *oneSecTimer;
+@property (nonatomic, retain) NSTimer *twoSecTimer;
+
+@property (nonatomic, retain) NSDate *zeroTimestamp;
+
+-(void) createTimer:(NSTimer *)timer withTimeInterval:(NSTimeInterval)timeInterval selector:(SEL)aSelector repeats:(BOOL)repeat;
+-(void) oneSecondRepeatingTimer; 
+-(void) twoSecondRepeatingTimer;
+-(void) updateLabel1;
+-(void) updateLabel2;
+-(IBAction) startButtonPressed:(id) sender;
+-(IBAction) stopButtonPressed:(id) sender;
+
 @end
--- TimerDebug/BadTimerViewController.m
+++ TimerDebug/BadTimerViewController.m
@@ -11,6 +11,13 @@
 
 @implementation BadTimerViewController
 
+@synthesize oneSecTimer;
+@synthesize twoSecTimer;
+
+@synthesize timer1Label;
+@synthesize timer2Label;
+@synthesize zeroTimestamp;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -22,6 +29,11 @@
 
 - (void)dealloc
 {
+    [zeroTimestamp release];
+    [oneSecTimer release];
+    [twoSecTimer release];
+    [timer1Label release];
+    [timer2Label release];
     [super dealloc];
 }
 
@@ -39,6 +51,8 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.timer1Label.text = @"0.0 sec";
+    self.timer2Label.text = @"0.0 sec";
 }
 
 - (void)viewDidUnload
@@ -58,6 +72,56 @@
 -(void) invalidateTimer:(NSTimer *)timer {
     [timer invalidate];
     timer = nil;
+}
+
+-(void) createTimer:(NSTimer *)timer withTimeInterval:(NSTimeInterval)timeInterval selector:(SEL)aSelector repeats:(BOOL)repeat {
+    // Get current runloop
+	NSRunLoop* runLoop = [NSRunLoop currentRunLoop];
+	
+    // Invalidate previously set NSTimer
+    [self invalidateTimer:timer];
+    
+    // Create new NSTimer
+	timer = [NSTimer scheduledTimerWithTimeInterval:timeInterval
+                                             target:self
+                                           selector:aSelector
+                                           userInfo:nil
+                                            repeats:repeat];
+    // Add NSTimer to runloop
+	[runLoop addTimer:timer forMode:NSDefaultRunLoopMode];    
+}
+
+-(void) oneSecondRepeatingTimer {     
+    [self createTimer:self.oneSecTimer withTimeInterval:1.0 selector:@selector(updateLabel1) repeats:YES];    
+}
+
+-(void) twoSecondRepeatingTimer { 
+    [self createTimer:self.twoSecTimer withTimeInterval:0.1 selector:@selector(updateLabel2) repeats:YES];    
+}
+
+-(void) updateLabel1 {
+    self.timer1Label.text = [NSString stringWithFormat:@"%.1f sec", [[NSDate date] timeIntervalSinceDate:self.zeroTimestamp]];
+}
+
+
+-(void) updateLabel2 {
+    self.timer2Label.text = [NSString stringWithFormat:@"%.1f sec", [[NSDate date] timeIntervalSinceDate:self.zeroTimestamp]];
+}
+
+-(IBAction) startButtonPressed:(id) sender {
+    self.zeroTimestamp = [NSDate date];
+    
+    self.timer1Label.text = @"0.0 sec";
+    self.timer2Label.text = @"0.0 sec";
+    
+//    [self oneSecondRepeatingTimer];
+    [self twoSecondRepeatingTimer];
+}
+
+-(IBAction) stopButtonPressed:(id) sender {
+    self.zeroTimestamp = nil;
+//    [self invalidateTimer:self.oneSecTimer];
+    [self invalidateTimer:self.twoSecTimer];
 }
 
 @end
